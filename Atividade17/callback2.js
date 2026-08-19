466d228be7310f9c94130f9ce24220bbfed0299e
Gustavo Lopes <164338321+lopxs1@users.noreply.github.com>
2026-08-19 14:32:41 -0300

    Add files via upload

---
+++ Atividade17/callback2.js
@@ -0,0 +1,7 @@
+function exibeMensagemNaOrdem(mensagem, callback){
+    console.log(mensagem);
+    callback();
+}
+exibeMensagemNaOrdem('Essa é a primeira mensagem exibida na ordem', function(){
+    console.log('Essa é a segunda mensagem exibida na ordem');
+});
